15c1f05b3b6af926dd6bad4d0968a1d36979bd35
Eudes Robin <eudes.robin@gmail.com>
2015-09-17 15:37:03 +0200

    fix linux acc

--- src/projects/tokenTree/nodes/nodeImplementations/tokenNode.java
+++ src/projects/tokenTree/nodes/nodeImplementations/tokenNode.java
@@ -1,6 +1,5 @@
 package projects.tokenTree.nodes.nodeImplementations;
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Iterator;
@@ -20,19 +19,18 @@ public class tokenNode extends Node {
 
 	public boolean passage =false;
 
-
 	public void preStep() {}
 
-	// ATTENTION lorsque init est appel� les liens de communications n'existent pas
-	// il faut attend une unit� de temps, avant que les connections soient r�alis�es
+	// ATTENTION lorsque init est appelé les liens de communications n'existent pas
+	// il faut attend une unité de temps, avant que les connections soient réalisées
 	// nous utilisons donc un timer
 
 	public void init() {
 		(new initTimer()).startRelative(1, this);
 	}
 
-	// Lorsque le timer pr�c�dent expire, la fonction start est appel�e
-	// elle correspond ainsi � l'initialisation r�elle du processus
+	// Lorsque le timer précédent expire, la fonction start est appelée
+	// elle correspond ainsi à l'initialisation réelle du processus
 
 	public void start(){
 		if(this.ID==1) {
@@ -41,21 +39,19 @@ public class tokenNode extends Node {
 		}
 	}
 
-	// Cette fonction g�re la r�ception de message
-	// Elle est appel�e r�guli�rement m�me si aucun message n'a �t� re�u
+	// Cette fonction gère la réception de message
+	// Elle est appelée régulièrement même si aucun message n'a été reçu
 
 	public void handleMessages(Inbox inbox) {
-		// Test si il y a des messages
-
 		while(inbox.hasNext())
 		{
 			Message m=inbox.next();
-			if(m instanceof tokenMessage) // Si le processus a re�u le jeton 
+			if(m instanceof tokenMessage)
 			{	
 				if ((this.ID == 1) && (getLabel(((tokenMessage) m).sender) == this.outgoingConnections.size())){
-					
+
 					Tools.stopSimulation();				
-					}
+				}
 				else{
 					this.passage = true;
 					send(new tokenMessage(this), getNode(getLabel(((tokenMessage) m).sender) %  this.outgoingConnections.size() + 1 ));
@@ -63,9 +59,9 @@ public class tokenNode extends Node {
 			}	
 		}
 	}
-	
+
 	private int getLabel(Node node){
-		
+
 		Iterator<Edge> liste = this.outgoingConnections.iterator();
 		int i = 1;
 		while(liste.hasNext()){
@@ -97,12 +93,9 @@ public class tokenNode extends Node {
 	}
 
 	// affichage du noeud
-
 	public void draw(Graphics g, PositionTransformation pt, boolean highlight){
 		this.setColor(this.Couleur());
 		String text = ""+this.ID;
 		super.drawNodeAsDiskWithText(g, pt, highlight, text, 20, Color.black);
 	}
-
-
 }
