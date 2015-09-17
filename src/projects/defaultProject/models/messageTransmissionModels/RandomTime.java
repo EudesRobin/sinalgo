41c0e0a61aed466321961971e36340e3a572204a
Eudes Robin <eudes.robin@gmail.com>
2015-09-17 15:24:14 +0200

    fix FifoRandom models + add jdom.jar

--- src/projects/defaultProject/models/messageTransmissionModels/RandomTime.java
+++ src/projects/defaultProject/models/messageTransmissionModels/RandomTime.java
@@ -34,7 +34,7 @@
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-package projects.defaultProject.models.messageTransmissionModels;
+package projects.tokenTree.models.messageTransmissionModels;
 
 import sinalgo.configuration.CorruptConfigurationEntryException;
 import sinalgo.nodes.Node;
